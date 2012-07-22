9c42e44375be5e960cda4fc7ba4465dea676bbf0
Matt Culpepper <culpepper.matthew@gmail.com>
2012-07-22 00:31:06 -0500

    remove debug

--- controllers/quote_controller.rb
+++ controllers/quote_controller.rb
@@ -5,9 +5,6 @@ class QuoteController < Rubot::Controller
 
   command :addquote, :addq do
     msg, author = Quote.parse(message.text)
-    reply "parsing #{message.text}"
-    reply "message = #{msg[1, 25]}"
-    reply "author = #{author}"
     Quote.add(msg, author)
   end
 end
