8839f5124e907d613d017bb527dd7e01bc282469
Justin Wheeler <jnwheeler44@gmail.com>
2012-09-21 10:38:23 -0400

    better tweet format

--- resources/twitter_util.rb
+++ resources/twitter_util.rb
@@ -2,7 +2,7 @@ class TwitterUtil
   require 'twitter'
   def self.tweet(status_id)
     if status = Twitter.status(status_id)
-      "'#{status.text}' - @#{status.user.screen_name}"
+      "\u0002@#{status.user.screen_name}: \u0002\u201C\u0016#{status.text}\u201D"
     end
   end
 end
