9601d4d302abd03f2359cfdb4a1ee6653ebd82ef
hunter haydel <haydh530@gmail.com>
2012-11-09 09:57:20 -0600

    izify goes live!

--- controllers/iz_controller.rb
+++ controllers/iz_controller.rb
@@ -1,6 +1,6 @@
 class IzController < Rubot::Controller
   command :izify do
-    reply "http://corpweb-dev.iseatz.com/izify/#{message.text}.png"
+    reply "http://www.iseatz.com/izify/#{message.text}.png"
   end
 end
 
