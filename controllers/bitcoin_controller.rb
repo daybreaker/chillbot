5d196dc1ca573b3e24604419c2e06658e3df00e6
Matt Culpepper <matt@culpepper.co>
2013-03-23 21:40:40 -0500

    remove extra $

--- controllers/bitcoin_controller.rb
+++ controllers/bitcoin_controller.rb
@@ -1,11 +1,11 @@
 class BitcoinController < Rubot::Controller
   on :connect do
     Scheduler.every "1h" do
-      server.message "#iz", "[bitcoin] 1 BTC = $#{Bitcoin.average_price}"
+      server.message "#iz", "[bitcoin] 1 BTC = #{Bitcoin.average_price}"
     end
   end
 
   command :btc do
-    reply "[bitcoin] 1 BTC = $#{Bitcoin.average_price}"
+    reply "[bitcoin] 1 BTC = #{Bitcoin.average_price}"
   end
 end
