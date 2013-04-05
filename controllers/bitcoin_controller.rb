140910d4771d23768506c96a3b1e7c4090f9f7d9
Matt Culpepper <matt@culpepper.co>
2013-04-04 21:41:10 -0500

    fucking is this a good time to fucking buy?

--- controllers/bitcoin_controller.rb
+++ controllers/bitcoin_controller.rb
@@ -1,7 +1,15 @@
 class BitcoinController < Rubot::Controller
   on :connect do
     Scheduler.every "1h" do
-      server.message "#iz", "[bitcoin] 1 BTC = #{Bitcoin.average_price} -- last price: #{Bitcoin.last_price}"
+      response = Bitcoin::Call.new
+
+      server.message "#iz", "[bitcoin] 1 BTC = #{response.average_price} -- last price: #{response.last_price}"
+
+      if response.really_good_time_to_buy?
+        server.message "#iz", "hey mculp, it's a *really* good time to buy."
+      elsif response.good_time_to_buy?
+        server.message "#iz", "hey mculp, it's a good time to buy."
+      end
     end
   end
 
