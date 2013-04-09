98c0eda1cd5d7220bd5ee0a78eb970804463afdc
Cameron Sampson <vaerros@gmail.com>
2013-04-08 19:42:01 -0500

    Added emoticon controller
    - Adds !lod, !shrug, !fliptable

---
+++ controllers/emoticon_controller.rb
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+
+class EmoticonController < Rubot::Controller
+  command :lod do
+    reply "ಠ_ಠ"
+  end
+
+  command :shrug do
+    reply "¯\\_(ツ)_/¯"
+  end
+
+  command :fliptable do
+    reply "(╯°□°）╯︵ ┻━┻"
+  end
+end
